ac1e1c6b83db5e873d57cb5dff5922aeb956eab8
unknown <rifqirm15122001@gmail.com>
2024-01-03 12:58:42 +0700

    Push Tugas Minggu Kedua BK

--- bk_poli-new-/resources/views/pasien/pilihpoli.blade.php
+++ bk_poli-new-/resources/views/pasien/pilihpoli.blade.php
@@ -82,16 +82,16 @@
                   <div class="w-full max-w-full px-3 mt-0 shrink-0 lg:w-1/2 lg:flex-none">
                     <ul class="flex flex-wrap justify-center pl-0 mb-0 list-none lg:justify-end">
                       <li class="nav-item">
-                        <a href="mailto:naufal.iksham@gmail.com" class="block px-4 pt-0 pb-1 text-sm font-normal transition-colors ease-soft-in-out text-slate-500" target="_blank">Email</a>
+                        <a href="mailto:rifqirm15122001@gmail.com" class="block px-4 pt-0 pb-1 text-sm font-normal transition-colors ease-soft-in-out text-slate-500" target="_blank">Email</a>
                       </li>
                       <li class="nav-item">
-                        <a href="https://www.instagram.com/naufal_iksham/" class="block px-4 pt-0 pb-1 text-sm font-normal transition-colors ease-soft-in-out text-slate-500" target="_blank">Instagram</a>
+                        <a href="https://www.instagram.com/rifqirm1512/" class="block px-4 pt-0 pb-1 text-sm font-normal transition-colors ease-soft-in-out text-slate-500" target="_blank">Instagram</a>
                       </li>
                       <li class="nav-item">
-                        <a href="https://www.facebook.com/naufaliksham/" class="block px-4 pt-0 pb-1 text-sm font-normal transition-colors ease-soft-in-out text-slate-500" target="_blank">Facebook</a>
+                        <a href="https://www.facebook.com/rifqi.r.m.54" class="block px-4 pt-0 pb-1 text-sm font-normal transition-colors ease-soft-in-out text-slate-500" target="_blank">Facebook</a>
                       </li>
                       <li class="nav-item">
-                        <a href="https://api.whatsapp.com/send/?phone=6282243090750&text&type=phone_number&app_absent=0" class="block px-4 pt-0 pb-1 pr-0 text-sm font-normal transition-colors ease-soft-in-out text-slate-500" target="_blank">WhatsApp</a>
+                        <a href="https://api.whatsapp.com/send/?phone=6282237914066&text&type=phone_number&app_absent=0" class="block px-4 pt-0 pb-1 pr-0 text-sm font-normal transition-colors ease-soft-in-out text-slate-500" target="_blank">WhatsApp</a>
                       </li>
                     </ul>
                   </div>
